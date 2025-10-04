c8f8c91a2f1029fed5add8f5a6ddbfeac6b2be50
LeonRado1 <radoleon2@gmail.com>
2025-10-05 00:23:38 +0200

    add login page

--- quasar-project/src/layouts/AuthLayout.vue
+++ quasar-project/src/layouts/AuthLayout.vue
@@ -1,8 +1,8 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-page-container>
-      <q-page class="flex flex-center q-my-xl">
-        <q-card class="q-pa-md shadow-2 rounded-xl">
+      <q-page class="flex flex-center">
+        <q-card class="q-pa-md shadow-2 rounded-xl" style="min-width: 400px">
           <q-tabs>
             <q-route-tab to="/auth/login" label="Login" exact />
             <q-route-tab to="/auth/signup" label="Signup" exact />
--- quasar-project/src/pages/LoginPage.vue
+++ quasar-project/src/pages/LoginPage.vue
@@ -1,9 +1,70 @@
 <template>
-  <q-page>
-    <!-- content -->
-  </q-page>
+  <q-form @submit.prevent="login" class="q-gutter-sm q-my-lg q-pt-md text-center">
+    <q-input
+      v-model="email"
+      outlined
+      label="Email"
+      type="email"
+      lazy-rules
+      :rules="[
+        (val) => (val && val.length > 0) || 'This field is required',
+        (val) => /^\S+@\S+\.\S+$/.test(val) || 'Invalid email format',
+      ]"
+    />
+    <q-input
+      v-model="password"
+      outlined
+      label="Password"
+      type="password"
+      lazy-rules
+      :rules="[(val) => (val && val.length > 0) || 'This field is required']"
+    />
+    <q-btn
+      class="q-mt-md"
+      type="submit"
+      color="primary"
+      label="Submit"
+      :disabled="!isFormValid()"
+    />
+  </q-form>
 </template>
 
-<script setup lang="ts">
-//
+<script lang="ts">
+import { Notify } from 'quasar';
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    login() {
+      console.log(this.email, this.password);
+      if (this.email.length > 10) {
+        this.$router.push('/');
+      } else {
+        Notify.create({
+          message: 'Invalid login credentials',
+          color: 'negative',
+          icon: 'error',
+          position: 'bottom-right',
+          actions: [
+            {
+              color: 'white',
+              rounded: true,
+              icon: 'close',
+              handler: () => {},
+            },
+          ],
+        });
+      }
+    },
+    isFormValid(): boolean {
+      const pattern = /^\S+@\S+\.\S+$/;
+      return this.email.length > 0 && this.password.length > 0 && pattern.test(this.email);
+    },
+  },
+};
 </script>
