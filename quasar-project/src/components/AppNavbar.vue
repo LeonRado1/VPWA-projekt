47ce881f8ad4971f765bd54e434dd7a8d41283c5
LeonRado1 <radoleon2@gmail.com>
2025-10-06 19:40:22 +0200

    change app navbar

--- quasar-project/src/components/AppNavbar.vue
+++ quasar-project/src/components/AppNavbar.vue
@@ -1,7 +1,29 @@
 <template>
-  <div>My component</div>
+  <q-header elevated>
+    <q-toolbar>
+      <q-btn flat round dense icon="menu" class="q-mr-sm" />
+      <img style="max-width: 7.5rem" alt="Threadly Logo" src="/logo-dark.svg" />
+      <q-space></q-space>
+      <span class="q-mr-md">{{ userStore.user?.email }}</span>
+      <q-btn @click="logout" outline label="Logout" />
+    </q-toolbar>
+  </q-header>
 </template>
 
-<script setup lang="ts">
-//
+<script lang="ts">
+import { useUserStore } from 'stores/user';
+
+export default {
+  data() {
+    return {
+      userStore: useUserStore(),
+    };
+  },
+  methods: {
+    logout() {
+      this.userStore.setUser(null);
+      this.$router.push('/auth');
+    },
+  },
+};
 </script>
